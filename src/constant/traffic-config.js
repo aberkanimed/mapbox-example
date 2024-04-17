15a03087ed2eafac99ca853a8ed81f7122775a91
alberto <rojaszarzaalberto@gmail.com>
2024-04-17 10:58:54 +0200

    último

--- src/constant/traffic-config.js
+++ src/constant/traffic-config.js
@@ -1,12 +1,20 @@
 const AI_RESPONSE = [
-    {
-        response: `After analyzing the traffic data, we've pinpointed several high incident areas in the city. Notably, the intersection of Main St. and Elm St., the Highway 101 exit ramp to downtown, and the bridge over River Ave. suffer from frequent accidents and congestion. These issues lead to significant delays and disruptions in traffic flow, especially during peak hours and special events. To address these challenges, recommendations include enhancing monitoring with traffic cameras, improving communication about road closures, and implementing infrastructure upgrades like dynamic signage and traffic calming measures. These actions aim to improve safety and alleviate congestion, ensuring smoother travel for commuters and residents.`
-    },
-    {
-        response: `Analyzing these incidents, we've noticed recurring patterns such as distracted driving, failure to yield, and inadequate signage or road markings contributing to the accidents. Additionally, adverse weather conditions and poor visibility have exacerbated the situation in some cases.
+  {
+    response: `After analyzing the traffic data, we've pinpointed 
+        several high incident areas in the city. Notably, the intersection 
+        of Alameda Principal Street and Andalucian Avenue suffer from frequent accidents and congestion. These issues lead to 
+        significant delays and disruptions in traffic flow, especially during peak hours and
+         special events. To address these challenges, recommendations include enhancing monitoring
+          with traffic cameras, improving communication about road closures, and implementing
+           infrastructure upgrades like dynamic signage and traffic calming measures.
+            These
+             actions aim to improve safety and alleviate congestion, ensuring smoother travel for commuters and residents.`,
+  },
+  {
+    response: `Analyzing these incidents, we've noticed recurring patterns such as distracted driving, failure to yield, and inadequate signage or road markings contributing to the accidents. Additionally, adverse weather conditions and poor visibility have exacerbated the situation in some cases.
 
-        By delving deeper into these patterns and trends, we can develop targeted interventions such as enhanced enforcement of traffic regulations, improvements in road infrastructure, and public awareness campaigns to address the root causes of these incidents and improve overall road safety.`
-    }
-]
+        By delving deeper into these patterns and trends, we can develop targeted interventions such as enhanced enforcement of traffic regulations, improvements in road infrastructure, and public awareness campaigns to address the root causes of these incidents and improve overall road safety.`,
+  },
+];
 
 export default AI_RESPONSE;
